bdea8f590ec818a3eb72ec82783cccb58f719f8e
marleyabe <marleyabe@gmail.com>
2020-05-14 17:58:03 -0300

    14-05

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+FAMILIARES = [
+	('1', 'Pai'),
+	('2', 'Mae')
+]
+
+class Familia(models.Model):
+    generos = models.CharField(choices=FAMILIARES, max_length=10)
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=200)
+    sobrenome = models.CharField(max_length=200)
+    parentesco = models.ForeignKey(Familia, on_delete=models.CASCADE)
+
