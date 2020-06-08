812221d53bde0abf457a02eac1ba96210a081046
mayconabe <maycon.abe01@gmail.com>
2020-06-08 15:31:34 -0300

    first commit

--- main/models.py
+++ main/models.py
@@ -1,18 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Arvore(models.Model):
-    
-    no = models.IntegerField()
-    noPai = models.IntegerField()
-
 class Pessoa(models.Model):
 
-    nome = models.CharField(max_length=50)
-    sobrenome = models.CharField(max_length=50)
-    dataNasc = models.DateTimeField()
-    falecido = models.BooleanField()
-    dataFalecimento = models.DateTimeField()
+    usuario_django = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_usuario = models.BooleanField()
     pai = models.CharField(max_length=50)
     mae = models.CharField(max_length=50)
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    imagem_url = models.CharField(max_length=50)
+    
+    def filhos(self):
+        pass
+
+    def conjuge(self):
+        pass
